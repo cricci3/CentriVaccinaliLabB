999e687fae29b10dc2820c1a51249bbcfccfd5a3
cricci3 <cricci3@studenti.uninsubria.it>
2022-05-03 21:38:49 +0200

    Aggiunta View add-centro-vaccinale.fxml

--- src/main/java/it/uninsubria/centrivaccinalilabb/Hello.java
+++ src/main/java/it/uninsubria/centrivaccinalilabb/Hello.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Hello extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Hello.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("CentriVaccinali");
         stage.setScene(scene);
