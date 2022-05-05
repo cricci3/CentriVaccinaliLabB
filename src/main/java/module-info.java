20043089c576f868ebf4135ce7a216deca76fa78
Alessandro Macrina <alessandro.macrina1997@gmail.com>
2022-05-05 14:48:40 +0200

    aggiunto file "module-info.java"

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module CentriVaccinaliLabB {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens it.uninsubria.centrivaccinalilabb to javafx.fxml;
+    exports it.uninsubria.centrivaccinalilabb;
+}
