64b9c16f5ef12065126325f1fcca3ff25f137e07
Alessandro Macrina <alessandro.macrina1997@gmail.com>
2022-05-06 12:05:49 +0200

    Implementato cambio di scena sulla schermata iniziale

--- src/main/java/it/uninsubria/centrivaccinalilabb/controllers/ControllerHelloCentriVaccinali.java
+++ src/main/java/it/uninsubria/centrivaccinalilabb/controllers/ControllerHelloCentriVaccinali.java
@@ -2,7 +2,6 @@ package it.uninsubria.centrivaccinalilabb.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,22 +10,26 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
-public class ControllerHelloCentriVaccinali {
+public class ControllerHelloCentriVaccinali{
     public Button btn_operatore, btn_cittadino;
     private Scene scene;
     private Stage stage;
     private Parent root;
 
     public void switchToOperatore(ActionEvent event){
-        String sceneFile = "resources/centrivaccinalilabb/hello-operatore.fxml";
-        URL url = getClass().getClassLoader().getResource(sceneFile);
+        String sceneFile = "/it/uninsubria/centrivaccinalilabb/hello-operatore.fxml";
+        URL url = getClass().getResource(sceneFile);
         try {
-            root = FXMLLoader.load(url);
-            System.out.println("  fxmlResource = " + sceneFile);
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
+            FXMLLoader fxmlLoader;
+
+            fxmlLoader = new FXMLLoader(getClass().getResource(sceneFile));
+            root = (Parent) fxmlLoader.load();
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
             stage.setScene(scene);
             stage.show();
+
         } catch (Exception e) {
             System.out.println( "Exception on FXMLLoader.load()" );
             System.out.println( "  * url: " + url );
@@ -36,14 +39,24 @@ public class ControllerHelloCentriVaccinali {
     }
 
     public void switchToCittadino(ActionEvent event) throws IOException {
+        String sceneFile = "/it/uninsubria/centrivaccinalilabb/hello-cittadino.fxml";
+        URL url = getClass().getResource(sceneFile);
         try {
-            root = FXMLLoader.load(getClass().getResource("hello-cittadino.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
+            FXMLLoader fxmlLoader;
+
+            fxmlLoader = new FXMLLoader(getClass().getResource(sceneFile));
+            root = (Parent) fxmlLoader.load();
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
             stage.setScene(scene);
             stage.show();
+
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println( "Exception on FXMLLoader.load()" );
+            System.out.println( "  * url: " + url );
+            System.out.println( "  * " + e );
+            System.out.println( "    ----------------------------------------\n" );
         }
     }
 }
